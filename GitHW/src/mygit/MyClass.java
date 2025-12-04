177782f7b896a544188bcfaba7478ff2a0502524
강두환 <@DESKTOP-23NV5MB>
2025-12-04 14:22:12 +0900

    first commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,7 +4,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");		
-		System.out.println("fssdf");		
+		System.out.println("helo+");
+		System.out.println("say eeeeehelo");
 
 	}	
 }
