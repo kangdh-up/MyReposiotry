0abf2a1d4d34556a8bf40e1ad1fded12b5df0089
강두환 <@DESKTOP-23NV5MB>
2025-12-04 14:27:10 +0900

    masterdptje 수정

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,7 +4,9 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");		
-		System.out.println("fssdf");		
+	
+		System.out.println("master 에서 수정");
+
 
 	}	
 }
--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -4,7 +4,9 @@ public class YourClass {
 
 	public YourClass() {
 		System.out.println("helo+");
-		
+		System.out.println("fssdfasdfasdfasfssdf");
+		System.out.println("master 에서 수정");
+
 		
 	}
 
